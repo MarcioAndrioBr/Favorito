aea06a09f2ca3dc364cc2ec612c2d04623724c58
Marcio Andrio <marcio_andrio@hotmail.com>
2025-02-28 08:25:49 +0000

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-// header.js
-document.addEventListener("scroll", function() {
-    var cabecalho = document.querySelector(".cabecalho");
-    if (window.scrollY > 50) { // Quando rolar mais de 50px
-        cabecalho.classList.add("solid");
-    } else {
-        cabecalho.classList.remove("solid");
-    }
-});
